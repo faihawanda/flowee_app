f7b47977d23a52e24cc3afd073885601923d99bb
Faiha Wanda <faihawandaa@gmail.com>
2026-09-23 10:56:59 +0700

    adding login brand header

---
+++ lib/widgets/login_brand_header.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LoginBrandHeader extends StatelessWidget {
+  const LoginBrandHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 78,
+          height: 78,
+          decoration: BoxDecoration(
+            color: Colors.white.withValues(alpha: 0.16),
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.white.withValues(alpha: 0.28),
+              width: 1.2
+            )
+          ),
+          child: Icon(
+            Icons.local_florist_rounded,
+            size: 40,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "Rangkaian bunga segar, dikirim dengan cinta",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white.withValues(alpha: 0.85),
+            fontSize: 13
+          ),
+        )
+      ],
+    );
+  }
+}
